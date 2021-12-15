5e3d194463704fc54d3ecd516949360d4acd58ad
Arturo-Lopez <jo.arturo.lopez.go@gmail.com>
2021-12-15 01:28:00 -0300

    refactor: change sync to async functions for passwordEncryption

--- api/src/lib/passwordEncryption.ts
+++ api/src/lib/passwordEncryption.ts
@@ -1,12 +1,12 @@
 import bcrypt from 'bcryptjs';
 
 export const getPasswordHash = async (password: string): Promise<string> => {
-  return bcrypt.hashSync(password, 10);
+  return bcrypt.hash(password, 10);
 };
 
 export const isValidPassword = async (
   password: string,
   passwordHash: string,
 ): Promise<boolean> => {
-  return bcrypt.compareSync(password, passwordHash);
+  return bcrypt.compare(password, passwordHash);
 };
